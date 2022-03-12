86a076685a7117729091072dda2668f81662d2a3
PEPPER0NY <90097190+PEPPER0NY@users.noreply.github.com>
2022-03-12 15:32:50 +0300

    Сам проект
    редорвор

---
+++ Chillout/DataAccess.Core/Models/HistoryMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chillout.DataAccess.Core.Models
+{
+    [Table("HistoryMovement")]
+    public class HistoryMovement
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string PlaceId { get; set; }
+        public UserRto Place { get; set; }
+
+        public int TimeId { get; set; }
+        public UserRto Time { get; set; }
+        public DateTime DateTimeStamp { get; set; }
+
+        public List<HistoryMovement> HistoryMovementPlace { get; set; }
+        public List<HistoryMovement> HistoryMovementTime { get; set; }
+        
+
+    }
+}
