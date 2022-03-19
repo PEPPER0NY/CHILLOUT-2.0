53a72c2472288398cb45d474be7ac4ed0f21ac9d
PEPPER0NY <90097190+PEPPER0NY@users.noreply.github.com>
2022-03-19 16:59:24 +0300

    ДОБАВИЛ МИГРАЦИЮ

--- Chillout/Migrations/InitialCreate.cs
+++ Chillout/Migrations/InitialCreate.cs
@@ -1,11 +1,84 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Chillout.Migrations
 {
-    public class InitialCreate
+    public partial class InitialCreate : Migration
     {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(nullable: true),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PassListRto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Website = table.Column<int>(nullable: false),
+                    Login = table.Column<int>(nullable: false),
+                    PassWord = table.Column<int>(nullable: false),
+                    AdditionMoney = table.Column<int>(nullable: false),
+                    DateTimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassList", x => x.Id);
+                    table.ForeignKey(
+                        name: "",
+                        column: x => x.Website,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "",
+                        column: x => x.Login,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "",
+                        column: x => x.PassWord,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoryGame_LeftUserId",
+                table: "HistoryGame",
+                column: "LeftUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoryGame_RightUserId",
+                table: "HistoryGame",
+                column: "RightUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassList");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+        }
     }
 }
