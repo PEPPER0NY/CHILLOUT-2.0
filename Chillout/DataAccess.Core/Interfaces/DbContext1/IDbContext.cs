73053de2fe2567cef2f16d1cba437b9f56658898
PEPPER0NY <90097190+PEPPER0NY@users.noreply.github.com>
2022-05-21 15:14:44 +0300

    фн
    5нф

---
+++ Chillout/DataAccess.Core/Interfaces/DbContext1/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Chillout.DataAccess.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chillout.DataAccess.Core.Interfaces.DbContext1
+{
+    public interface IDbContext : IAsyncDisposable
+    {
+        public DbSet<UserRto> User { get; set; }
+        public DbSet<PassListRto> PassList { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Chillout/DataAccess/Context/RubicContext.cs
@@ -0,0 +1,20 @@
+﻿using Chillout.DataAccess.Core.Interfaces.DbContext1;
+using Chillout.DataAccess.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chillout.DataAccess.Context
+{
+    public class RubicContext : DbContext, IDbContext
+    {
+        public RubicContext(DbContextOptions<RubicContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<UserRto> User { get; set; }
+        public DbSet<PassListRto> PassList { get; set; }
+
+    }
+}
